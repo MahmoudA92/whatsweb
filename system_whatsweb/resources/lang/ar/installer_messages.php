062eca498bdc8ec47aab4f32c1f740591181e61f
Mahmoud Adel <46160879+MahmoudA92@users.noreply.github.com>
2021-10-09 04:06:22 +0200

    Create installer_messages.php

---
+++ system_whatsweb/resources/lang/ar/installer_messages.php
@@ -0,0 +1,208 @@
+<?php
+
+return [
+    /**
+     *
+     * NavBar translations.
+     *
+     */
+
+    "wttsy" => "WTTSY",
+    "home" => "الصفحة الرئيسية",
+    "about us" => "من نحن",
+    "contact us" => "تواصل معنا",
+    'already have an account?' => 'هل لديك حساب؟',
+    'login' => 'تسجيل دخول',
+    'hi' => 'مرحباً',
+    'dashboard' => 'صفحة التحكم',
+    'logout' => 'تسجيل خروج',
+    'faq' => 'الأسئلة الشائعه',
+    'links' => 'الروابط',
+    'reports' => 'التقارير',
+    'report' => 'التقرير',
+    'settings' => 'الإعدادات',
+
+    /**
+     *
+     * Welcome Page translations.
+     *
+     */
+
+    "whatsapp click to chat generator" => "عمل رابط واتساب",
+    "create your own whatsapp click to chat easily, or shorten your url, and you can see the statistics!" => "قم بإنشاء WhatsApp الخاص بك انقر للدردشة بسهولة ، ويمكنك الاطلاع على الإحصائيات!",
+    "create an account" => "إنشاء حساب",
+    "create new link" => "إنشاء رابط جديد",
+    "whatsapp" => "رابط الواتساب",
+    "shorten link" => "تقصير الرابط",
+    "phone code" => "رمز الهاتف",
+    "phone number" => "رقم الهاتف",
+    "message" => "الرسالة الإفتراضيه ( إختياري )",
+    "your message here" => "رسالتك هنا",
+    "learn more about" => "تعلم المزيد عن",
+    "formatting your message" => "تنسيق رسالتك",
+    "on whatsapp" => "علي الواتساب",
+    "url" => "العنوان",
+    "the url you want to shorten" => "العنوان المراد اختصاره",
+    "custom slug" => "تعريف مخصص",
+    "by default, we generate a unique url. however, you can use your own character for human-readable urls" => "بشكل افتراضي ، نقوم بإنشاء عنوان فريد. ومع ذلك ، يمكنك كتابة إسم المستخدم الخاص بك للعناوين التي يمكن للبشر قراءتها",
+    "create link" => "إنشاء رابط",
+    "view last generated link" => "عرض آخر رابط تم إنشاؤه",
+    "get your link statistics by" => "احصل على إحصائيات الروابط الخاصة بك عن طريق",
+    "receiver" => "إستقبال",
+    "sender" => "إرسال",
+    "your link generated successfully" => "تم إنشاء الرابط بنجاح",
+    "raw" => "رابط",
+    "html" => "HTML",
+    "qr code" => "QR Code",
+    "share" => "مشاركة",
+    "copy the following url, and paste it wherever you like" => "انسخ العنوان التالي ، والصقه أينما تريد",
+    "copy" => "انسخ",
+    "download the following qr Code image" => "قم بتنزيل صورة رمز الاستجابة السريعة التالية",
+    "download image" => "تحميل الصورة",
+    "you can share this link with your social media account" => "يمكنك مشاركة هذا الرابط مع حساب وسائل التواصل الاجتماعي الخاص بك",
+    "facebook" => "الفيسبوك",
+    "twitter" => "تويتر",
+    "whatsApp" => "الواتساب",
+    "telegram" => "التليجرام",
+    "" => "",
+
+
+
+    /**
+     *
+     * Footer translations.
+     *
+     */
+
+    "loveSaudia" => "صنع بـ ❤️ في السعودية لجميع الناطقين بلغة الضاد الجميلة",
+    "copyright" => "حقوق النشر",
+    "WhatsWeb" => "واتسي",
+
+    /**
+     *
+     * Auth Pages translations.
+     *
+     */
+
+    "email" => "البريد الإلكتروني",
+    "password" => "الرقم السري",
+    "forgot password ?" => "هل نسيت كلمة السر ؟",
+    "remember me" => "تذكرني",
+    "login with social" => "تسجيل الدخول مع وسائل التواصل الاجتماعي",
+    "facebook" => "الفيسبوك",
+    "google" => "الجوجل",
+    "dont have an account ?" => "ليس لديك حساب ؟",
+    "create one" => "اصنع واحدا",
+    "register" => "التسجيل",
+    "name" => "الإسم",
+    "confirm password" => "تأكيد الرقم السري",
+    "agree with" => "اتفق مع",
+    "terms and conditions" => "الأحكام والشروط",
+    "you must agree with our terms and conditions" => "يجب أن توافق على الشروط والأحكام الخاصة بنا",
+
+    /**
+     *
+     * User Dashboard translations.
+     *
+     */
+    "total link" => "إجمالي الروابط",
+    "today visit" => "زيارات اليوم",
+    "yesterday visit" => "زيارات الأمس",
+    "7 days visit" => "زيارات 7 ايام",
+    "last 7 days" => "اخر 7 ايام",
+    "top 5 links" => "أهم 5 روابط",
+    "all links" => "جميع الروابط",
+    "shortlink" => "إختصار رابط موقع",
+    "create new link" => "إنشاء رابط جديد",
+    "hit" => "التسديد",
+    "type" => "النوع",
+    "created at" => "أنشئت في",
+    "action" => "العمل",
+    "no data" => "لا يوجد بيانات",
+    "choose date" => "اختر موعدا",
+    "link" => "رابط",
+    "all" => "الجميع",
+    "filter" => "الترشيح",
+    "chart" => "الجدول",
+    "referer" => "المُحيل",
+    "device" => "الجهاز",
+    "platform" => "البرنامج",
+    "browser" => "المتصفح",
+    "no" => "لأ",
+    "settings" => "الإعدادات",
+    "email cant be changed" => "لا يمكن تغيير البريد الإلكتروني",
+    "leave if not change" => "اتركه إذا لم يتغير",
+    "save changes" => "احفظ التغييرات",
+    "" => "",
+    "" => "",
+    "" => "",
+    "" => "",
+    "" => "",
+
+    /********************************************************************************/
+
+    /**
+     *
+     * Shared translations.
+     *
+     */
+
+    'title' => 'تنصيب Laravel',
+    'next' => 'متابعة',
+
+    /**
+     *
+     * Home page translations.
+     *
+     */
+
+    'welcome' => [
+        'title'   => 'تنصيب Laravel',
+        'message' => 'أهلا بك في صفحة تنصيب Laravel',
+    ],
+
+    /**
+     *
+     * Requirements page translations.
+     *
+     */
+
+    'requirements' => [
+        'title' => 'المتطلبات',
+    ],
+
+    /**
+     *
+     * Permissions page translations.
+     *
+     */
+
+    'permissions' => [
+        'title' => 'تراخيص المجلدات',
+    ],
+
+    /**
+     *
+     * Environment page translations.
+     *
+     */
+
+    'environment' => [
+        'title' => 'الإعدادات',
+        'save' => 'حفظ ملف .env',
+        'success' => 'تم حفظ الإعدادات بنجاح',
+        'errors' => 'حدث خطأ أثناء إنشاء ملف .env. رجاءا قم بإنشاءه يدويا',
+    ],
+    /**
+     *
+     * Final page translations.
+     *
+     */
+
+    'final' => [
+        'title' => 'النهاية',
+        'finished' => 'تم تنصيب البرنامج بنجاح...',
+        'exit' => 'إضغط هنا للخروج',
+    ],
+];
+
